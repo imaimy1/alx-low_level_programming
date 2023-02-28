52f0ddce40d64cf33331bf1fa698190d62150933
halima <halima@example.com>
2023-02-28 09:15:50 -0800

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,16 @@
 /*
- * File: 3-puts.c
+ *File: 3-puts.c
  * Auth:halima
  */
+#include <stddef.h>
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+
 void _puts(char *str)
 {
 	while (*str)
 		_putchar(*str++);
-
 	_putchar('\n');
 }
