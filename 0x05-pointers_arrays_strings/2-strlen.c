9b0954742a0ccc7787ae88950e9bdf82a5c76027
halima <halima@example.com>
2023-02-28 09:00:51 -0800

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,16 @@
 /*
  * File: 2-strlen.c
- * Auth:  halima
+ * Auth: halima
  */ 
-size_t_strlen(const char *str)
 
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+size_t _strlen(const char *str)
+{
 	size_t length = 0;
 
 	while (*str++)
