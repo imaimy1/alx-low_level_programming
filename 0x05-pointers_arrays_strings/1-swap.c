dce13fa9fb40ea035e8abda8d1ced08e68d132c1
halima <halima@example.com>
2023-02-28 08:55:02 -0800

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,6 +3,12 @@
  * File: 1-swap.c
  * Auth: halima
  */
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: The first integer to be swapped.
+ * @b: The second integer to be swapped.
+ */
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
