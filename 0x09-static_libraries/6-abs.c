c688aadf687e83132989d2becf539d209f4e7c37
imaimy1 <you@example.com>
2023-06-13 14:41:16 -0700

    add libmy

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int _abs(int n)
+{
+
+    return abs(n);
+
+}
+
+
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c)
+{
+
+	return putchar(c);
+
+}
+
