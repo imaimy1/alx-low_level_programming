201960b4a173b20e0752bf82a81f325cc68cde65
imaimy1 <you@example.com>
2023-06-13 16:09:34 -0700

    add function

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h> 
+
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: input
+ * @s2: input
+ * Return: 0
+ */
+
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int size1 = 0, size2 = 0;
+	char *x, *y;
+
+	x = s1;
+	if (s1)
+		while (*x++)
+			size1++;
+	else
+		s1 = "";
+
+	x = s2;
+	if (s2)
+		while (*x++)
+			size2++;
+	else
+		s2 = "";
+
+	y = malloc(size1 + size2 + 1);
+	if (!y)
+		return (NULL);
+
+	x = y;
+	while (*s1)
+		*x++ = *s1++;
+	while (*s2)
+		*x++ = *s2++;
+	*x = 0;
+
+	return (y);
+}
