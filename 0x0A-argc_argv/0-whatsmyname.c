1d66760f05bad44c44b57241aac874d2bb01c00c
imaimy1 <you@example.com>
2023-06-14 17:51:30 -0700

    add

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints it's  name.
+ * @argc: Number of  arguments
+ * @argv: Array.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
