c4ec7283d81fdf413bc7f54da14f1f3e3fa0cec8
imaimy1 <you@example.com>
2023-06-19 16:23:29 -0700

    add

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog.
+ * @d: a dog
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+		return;
+	if (d->owner)
+                free(d->owner);
+        free(d);
+	if (d->name)
+		free(d -> name);
+}
